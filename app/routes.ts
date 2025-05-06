8854c63ff5797fe7908b09076b3e8edb2e291756
ThiagoFang <thiagofang@gmail.com>
2025-05-06 14:48:03 -0300

    route files pattern

--- app/routes.ts
+++ app/routes.ts
@@ -2,7 +2,7 @@ import { type RouteConfig, layout, route } from "@react-router/dev/routes";
 
 export default [
 	layout("routes/admin/admin-layout.tsx", [
-		route("dashboard", "routes/admin/Dashboard.tsx"),
-		route("all-users", "routes/admin/AllUsers.tsx"),
+		route("dashboard", "routes/admin/dashboard.tsx"),
+		route("all-users", "routes/admin/all-users.tsx"),
 	]),
 ] satisfies RouteConfig;
